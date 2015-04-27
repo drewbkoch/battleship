7978f8193239035eca38ee74ee74902f5d733770
Drew Koch <dkoch13@gmail.com>
2015-04-27 14:13:24 -0700

    initial commit and basic data structure set up

---
+++ main.js
@@ -0,0 +1,51 @@
+console.log('main.js here...')
+
+
+//define data structure
+var player1 = {
+  "aircraftCarrier": {
+    "length": 5,
+    "coordinates": ["A1","A2","A3","A4","A5"]
+  },
+  "battleship": {
+    "length": 4,
+    "coordinates": ["B1","B2","B3","B4"]
+  },
+  "submarine": {
+    "length": 3,
+    "coordinates": ["C1","C2","C3"]
+  },
+  "destroyer": {
+    "length": 3,
+    "coordinates": ["D1","D2","D3"]
+  },
+  "patrolBoat": {
+    "length": 2,
+    "coordinates": ["E1","E2"]
+  }
+}
+
+var player2 = {
+  "aircraftCarrier": {
+    "length": 5,
+    "coordinates": ["A1","A2","A3","A4","A5"]
+  },
+  "battleship": {
+    "length": 4,
+    "coordinates": ["B1","B2","B3","B4"]
+  },
+  "submarine": {
+    "length": 3,
+    "coordinates": ["C1","C2","C3"]
+  },
+  "destroyer": {
+    "length": 3,
+    "coordinates": ["D1","D2","D3"]
+  },
+  "patrolBoat": {
+    "length": 2,
+    "coordinates": ["E1","E2"]
+  }
+}
+
+console.log(player1.aircraftCarrier.length);
